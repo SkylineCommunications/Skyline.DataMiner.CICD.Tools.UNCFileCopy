419e65567c32da5eea47d4d0a7a4bae12b380476
Michiel Oda <michiel.oda@skyline.be>
2024-03-12 13:51:47 +0100

    Adding editorconfig + adding 'SupportedOSPlatform' to remove SonarCloud error.

--- CICD.Tools.UNCFileCopy/UserImpersonation.cs
+++ CICD.Tools.UNCFileCopy/UserImpersonation.cs
@@ -1,66 +1,64 @@
 ﻿namespace Skyline.DataMiner.CICD.Tools.UNCFileCopy
 {
-	using System;
-	using System.Runtime.InteropServices;
-	using System.Security.Principal;
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Security.Principal;
 
-	using Microsoft.Win32.SafeHandles;
+    using Microsoft.Win32.SafeHandles;
 
-	/// <summary>
-	/// Allows for user impersonation in Windows to perform actions under a different user account.
-	/// </summary>
-	internal class UserImpersonation
-	{
-		private readonly string _domain;
+    /// <summary>
+    /// Allows for user impersonation in Windows to perform actions under a different user account.
+    /// </summary>
+    internal class UserImpersonation
+    {
+        private readonly string domain;
+        private readonly string password;
+        private readonly string username;
 
-		private readonly string _password;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserImpersonation"/> class.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="domain">The domain.</param>
+        /// <param name="password">The password.</param>
+        /// <exception cref="ArgumentException">Thrown when a required argument is null or empty.</exception>
+        public UserImpersonation(string username, string domain, string password)
+        {
+            if (String.IsNullOrWhiteSpace(username)) throw new ArgumentException("User name must not be null or empty.", nameof(username));
+            if (String.IsNullOrWhiteSpace(domain)) throw new ArgumentException("Domain must not be null or empty.", nameof(domain));
+            if (String.IsNullOrWhiteSpace(password)) throw new ArgumentException("Password must not be null or empty.", nameof(password));
 
-		private readonly string _userName;
+            this.username = username;
+            this.domain = domain;
+            this.password = password;
+        }
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="UserImpersonation"/> class.
-		/// </summary>
-		/// <param name="userName">The user name.</param>
-		/// <param name="domain">The domain.</param>
-		/// <param name="password">The password.</param>
-		/// <exception cref="ArgumentException">Thrown when a required argument is null or empty.</exception>
-		public UserImpersonation(string userName, string domain, string password)
-		{
-			if (string.IsNullOrWhiteSpace(userName)) throw new ArgumentException("User name must not be null or empty.", nameof(userName));
-			if (string.IsNullOrWhiteSpace(domain)) throw new ArgumentException("Domain must not be null or empty.", nameof(domain));
-			if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Password must not be null or empty.", nameof(password));
+        /// <summary>
+        /// Executes a given action as the impersonated user.
+        /// </summary>
+        /// <param name="action">The action to execute.</param>
+        /// <exception cref="ArgumentNullException">Thrown if the action is null.</exception>
+        /// <exception cref="UnauthorizedAccessException">Thrown if user logon fails.</exception>
+        public void RunAsUser(Action action)
+        {
+            if (action == null) throw new ArgumentNullException(nameof(action));
 
-			_userName = userName;
-			_domain = domain;
-			_password = password;
-		}
+            const int logon32ProviderDefault = 0;
+            const int logon32LogonInteractive = 2;
 
-		/// <summary>
-		/// Executes a given action as the impersonated user.
-		/// </summary>
-		/// <param name="action">The action to execute.</param>
-		/// <exception cref="ArgumentNullException">Thrown if the action is null.</exception>
-		/// <exception cref="UnauthorizedAccessException">Thrown if user logon fails.</exception>
-		public void RunAsUser(Action action)
-		{
-			if (action == null) throw new ArgumentNullException(nameof(action));
+            bool returnValue = LogonUser(username, domain, password, logon32LogonInteractive, logon32ProviderDefault, out SafeAccessTokenHandle safeAccessTokenHandle);
+            if (!returnValue)
+            {
+                throw new UnauthorizedAccessException("User logon failed. Check username, password, and domain.");
+            }
 
-			const int LOGON32_PROVIDER_DEFAULT = 0;
-			const int LOGON32_LOGON_INTERACTIVE = 2;
+            using (safeAccessTokenHandle)
+            {
+                WindowsIdentity.RunImpersonated(safeAccessTokenHandle, action);
+            }
+        }
 
-			bool returnValue = LogonUser(_userName, _domain, _password, LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT, out SafeAccessTokenHandle safeAccessTokenHandle);
-			if (!returnValue)
-			{
-				throw new UnauthorizedAccessException("User logon failed. Check username, password, and domain.");
-			}
-
-			using (safeAccessTokenHandle)
-			{
-				WindowsIdentity.RunImpersonated(safeAccessTokenHandle, action);
-			}
-		}
-
-		[DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-		private static extern bool LogonUser(string username, string domain, string password, int logonType, int logonProvider, out SafeAccessTokenHandle token);
-	}
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        private static extern bool LogonUser(string username, string domain, string password, int logonType, int logonProvider, out SafeAccessTokenHandle token);
+    }
 }
